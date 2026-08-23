d2bac415a9579b6fdecaec1a274f49f4d32c644d
Tokke2 <tokke2@gmail.com>
2026-08-23 10:35:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -22,7 +22,7 @@
    ============================================================ */
 'use strict';
 
-const VERSION = 'maskinkok-v75';
+const VERSION = 'maskinkok-v76';
 const CACHE_CORE = VERSION + '-core';     // assets + startsida (förcachas)
 const CACHE_PAGES = VERSION + '-pages';   // recept & övriga sidor
 const CACHE_IMG = VERSION + '-img';       // bilder
