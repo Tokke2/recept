87eb4d96b86360827786e2b8289fa451a26cb5a8
Tokke2 <tokke2@gmail.com>
2026-08-23 11:40:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -22,7 +22,7 @@
    ============================================================ */
 'use strict';
 
-const VERSION = 'maskinkok-v81';
+const VERSION = 'maskinkok-v82';
 const CACHE_CORE = VERSION + '-core';     // assets + startsida (förcachas)
 const CACHE_PAGES = VERSION + '-pages';   // recept & övriga sidor
 const CACHE_IMG = VERSION + '-img';       // bilder
@@ -59,6 +59,7 @@ const CORE = [
   'assets/etikett.js',
   'assets/tydlig.js',
   'assets/maskinlank.js',
+  'assets/hem.js',
   'json/donation.json',
   'json/affiliate.json',
   'json/maskindatabas.json',
