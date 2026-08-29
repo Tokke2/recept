1cdd2ab08bb4919b0d367410a219ebdd64483a67
Tokke2 <tokke2@gmail.com>
2026-08-29 14:46:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -22,7 +22,7 @@
    ============================================================ */
 'use strict';
 
-const VERSION = 'maskinkok-v94';
+const VERSION = 'maskinkok-v101';
 const CACHE_CORE = VERSION + '-core';     // assets + startsida (förcachas)
 const CACHE_PAGES = VERSION + '-pages';   // recept & övriga sidor
 const CACHE_IMG = VERSION + '-img';       // bilder
@@ -66,6 +66,7 @@ const CORE = [
   'json/maskindatabas.json',
   'json/energi.json',
   'json/maskiner-index.json',
+  'images/logo-mark.svg',
   'manifest.json'
 ];
 
